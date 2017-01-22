080a7b81579139768380a1745a64d297f3e9c535
Jinbo Wang <jinbwan@microsoft.com>
2017-01-22 13:20:05 +0800

    upload assets to github release

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,107 @@ const path = require('path');
 const async = require('async');
 const cliArgs = require('yargs').argv;
 const linuxDistro = require('linux-distro');
+const github = require('octonode');
+const git = require('simple-git')();
+
+function getRepoName(gitUrl) {
+    const fields = gitUrl.split(':');
+    if (fields.length < 2) return '';
+    const position = fields[1].length - '.git'.length;
+    const lastIndex = fields[1].lastIndexOf('.git');
+    if (lastIndex !== -1 && lastIndex === position) {
+        return fields[1].substring(0, position);
+    } else {
+        return fields[1];
+    }
+}
+
+function uploadAssets(client, tagName, filePath, distName, callback) {
+    async.waterfall([
+        // parse repo name from git repository configuration
+        (callback) => {
+            git.listRemote(['--get-url'], function(err, data) {
+                if (!err) {
+                    console.log('Remote url for repository at ' + __dirname + ':');
+                    const repoName = getRepoName(data.trim());
+                    console.log(repoName);
+                    if (repoName) {
+                        callback(null, repoName);
+                    } else {
+                        callback('Cannot get repo name for this repository.');
+                    }
+                } else {
+                    callback(err);
+                }
+            });
+        },
+        // get release by tag
+        (repoName, callback) => {
+            client.get(`/repos/${repoName}/releases/tags/${tagName}`, (err, res, body) => {
+                if (!err) {
+                    console.log(`release id: ${body.id}`);
+                    callback(null, repoName, body.id);
+                } else {
+                    callback(`The release via tag ${tagName} not found!`);
+                }
+            });
+        },
+        // check if asset exist or not.
+        (repoName, releaseId, callback) => {
+            client.get(`/repos/${repoName}/releases/${releaseId}/assets`, (err, res, body) => {
+                if (!err) {
+                    const find = body.find((element) => {
+                        return element.name === distName;
+                    });
+                    if (find) {
+                        console.log(`Finded an existing asset '${distName} in github release and delete it first.`);
+                        client.del(`/repos/${repoName}/releases/assets/${find.id}`, null, (err1, res1, body1) => {
+                            if (err1) {
+                                callback(`Cannot delete assets '${distName}'. See the error '${err1}'`);
+                            } else {
+                                console.log(`Finded an existing asset '${distName} in github release and delete it first.`);
+                                callback(null, repoName, releaseId);
+                            }
+                        });
+                    } else {
+                        callback(null, repoName, releaseId);
+                    }
+                } else {
+                    callback(null, repoName, releaseId, null);
+                }
+            });
+        },
+        // upload assets to releases.
+        (repoName, releaseId, callback) => {
+            const ghRelease = client.release(repoName, releaseId);
+            const archive = fs.readFileSync(filePath);
+            ghRelease.uploadAssets(archive, {
+                name: distName,
+                contentType: 'application/octet-stream',
+                uploadHost: 'uploads.github.com',
+            }, (err, res, body) => {
+                console.log(err);
+                console.log(res);
+                console.log(body);
+                if (!err) {
+                    console.log(`Succeeded to upload assets '${filePath}' to github release '${tagName}'`);
+                }
+                callback(err);
+            });
+        }
+    ], (error, results) => {
+        callback(error);
+    });
+}
 
 gulp.task('buildDll', (done) => {
+    if (!cliArgs.repoUrl || !cliArgs.token || !cliArgs.tag) {
+        done('Missing repoUrl, token, tag parameters!');
+        return ;
+    }
+    const client = github.client(cliArgs.token);
+    const tagName = cliArgs.tag;
+
     async.waterfall([
         // Pulling serial port package source code from GITHUB.
         (callback) => {
@@ -48,19 +147,21 @@ gulp.task('buildDll', (done) => {
                             callback('[node-gyp] Compiling serial port native code failed.');
                         } else {
                             console.log('[node-gyp] Build complete.');
-                            console.log(`Generate dll at ${path.normalize('./tmp/serialport/build/Release/serialport.node')}`);
+                            console.log(`Generate dll at ${path.normalize('./tmp/serialport/vendor/serialport-native/build/Release/serialport-native.node')}`);
                             if (platform === 'linux') {
                                 linuxDistro().then(data => {
                                     const packageName = `serialport_${data.os}${data.release || data.code}_${electron}_${arch}.node`;
                                     console.log(packageName);
-                                    callback();
+                                    uploadAssets(client, tagName, path.normalize('./tmp/serialport/vendor/serialport-native/build/Release/serialport-native.node'), packageName, callback);
                                 }, () => {
                                     const packageName = `serialport_${platform}_${electron}_${arch}.node`;
                                     console.log(packageName);
-                                    callback();
+                                    uploadAssets(client, tagName, path.normalize('./tmp/serialport/vendor/serialport-native/build/Release/serialport-native.node'), packageName, callback);
                                 });
                             } else {
-                                callback();
+                                const packageName = `serialport_${platform}_${electron}_${arch}.node`;
+                                console.log(packageName);
+                                uploadAssets(client, tagName, path.normalize('./tmp/serialport/vendor/serialport-native/build/Release/serialport-native.node'), packageName, callback);
                             }
                             // console.log('[azure-blob] Starting to upload build package to azure blob.');
                             // uploadFile(path.normalize('./tmp/serialport/build/Release/serialport.node'), `serialport_${platform}_${electron}_${arch}.node`, callback);
@@ -88,10 +189,6 @@ gulp.task('buildDll', (done) => {
             async.series(tasks, callback);
         },
     ], (error, result) => {
-        if (error) {
-            done(error);
-        } else {
-            done();
-        }
+        done(error);
     });
 });
